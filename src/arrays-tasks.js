b7bb5b9fd59169974aecd1898c6006e473caa04a
Vitali86 <vitali86@tut.by>
2024-02-08 12:57:39 +0300

    feat: findLongestIncreasingSubsequence

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -541,8 +541,19 @@ function findCommonElements(arr1, arr2) {
  *    findLongestIncreasingSubsequence([3, 10, 2, 1, 20]) => 2
  *    findLongestIncreasingSubsequence([50, 3, 10, 7, 40, 80]) => 3
  */
-function findLongestIncreasingSubsequence(/* nums */) {
-  throw new Error('Not implemented');
+function findLongestIncreasingSubsequence(nums) {
+  let max = 0;
+  let current = 1;
+  nums.every((el, i) => {
+    if (el < nums[i + 1]) {
+      current += 1;
+      if (current > max) max = current;
+    } else {
+      current = 1;
+    }
+    return true;
+  });
+  return max;
 }
 
 /**
